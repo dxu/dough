74f6314798da4761728f14356df6775854eeffde
David <xudavidb@gmail.com>
2016-08-05 16:42:34 -0700

    add movement

--- js/client/entry.js
+++ js/client/entry.js
@@ -3,48 +3,118 @@ import * as util from './utilities'
 
 util.ready(function(){
 
+const canvas = document.getElementById('canvas')
 // You can use either `new PIXI.WebGLRenderer`, `new PIXI.CanvasRenderer`, or `PIXI.autoDetectRenderer`
 // which will try to choose the best renderer for the environment you are in.
-var renderer = new PIXI.WebGLRenderer(800, 600);
+var renderer = new PIXI.WebGLRenderer(800, 600, {
+  view: canvas
+});
 
-// The renderer will create a canvas element for you that you can then insert into the DOM.
-document.body.appendChild(renderer.view);
+// Declare a global variable for our sprite so that the animate function can access it.
+var zelda = null;
+
+renderer.backgroundColor = 0xFFFFFF;
+
+canvas.focus()
+
+canvas.addEventListener('click', function(evt) {
+  if (this !== document.activeElement) {
+    canvas.focus()
+  }
+})
+
+canvas.addEventListener('keydown', function(evt) {
+  console.log('yo')
+  console.log(evt.keyCode)
+
+  switch (evt.keyCode)  {
+    case util.keys.W:
+      speed.y = -5
+      break
+    case util.keys.A:
+      speed.x = -5
+      break
+    case util.keys.S:
+      speed.y = 5
+      break
+    case util.keys.D:
+      speed.x = 5
+      break
+    default:
+      break
+  }
+
+  console.log(speed)
+
+  if (evt.keyCode === util.keys.A) {
+
+  }
+})
+
+var speed = {
+  x: 0,
+  y: 0
+}
+
+canvas.addEventListener('keyup', function(evt) {
+  console.log('yo')
+  console.log(evt.keyCode)
+
+  switch (evt.keyCode)  {
+    case util.keys.W:
+    case util.keys.A:
+    case util.keys.S:
+    case util.keys.D:
+      speed = {
+        x: 0,
+        y: 0
+      }
+      break;
+    default:
+      break
+  }
+  console.log(zelda.position.y,zelda.position.x)
+})
 
 // You need to create a root container that will hold the scene you want to draw.
 var stage = new PIXI.Container();
 
 
-// Declare a global variable for our sprite so that the animate function can access it.
-var zelda = null;
-
 // load the texture we need
-PIXI.loader.add('zelda', './assets/img/zelda.png').load(function (loader, resources) {
-    // This creates a texture from a 'zelda.png' image.
-    zelda = new PIXI.Sprite(resources.zelda.texture);
+PIXI.loader.add('zelda', './assets/img/zelda.gif').load(function (loader, resources) {
+  // This creates a texture from a 'zelda.png' image.
+  zelda = new PIXI.Sprite(resources.zelda.texture);
 
-    // Setup the position and scale of the zelda
-    zelda.position.x = 400;
-    zelda.position.y = 300;
+  // Setup the position and scale of the zelda
+  zelda.position.x = 400;
+  zelda.position.y = 300;
 
-    zelda.scale.x = 2;
-    zelda.scale.y = 2;
+  zelda.anchor.x = 0.5;
+  zelda.anchor.y = 0.5;
 
-    // Add the zelda to the scene we are building.
-    stage.addChild(zelda);
+  zelda.scale.x = 2;
+  zelda.scale.y = 2;
 
-    // kick off the animation loop (defined below)
-    animate();
+  // Add the zelda to the scene we are building.
+  stage.addChild(zelda);
+
+  // kick off the animation loop (defined below)
+  animate();
 });
 
 function animate() {
-    // start the timer for the next animation loop
-    requestAnimationFrame(animate);
+  // start the timer for the next animation loop
+  requestAnimationFrame(animate);
 
-    // each frame we spin the zelda around a bit
-    zelda.rotation += 0.01;
+  // each frame we spin the zelda around a bit
+  zelda.rotation += 0.01;
+  console.log(speed)
+  zelda.position.y += 1 * speed.y;
+  zelda.position.x += 1 * speed.x;
+  // zelda.position.y += 5
 
-    // this is the main render call that makes pixi draw your container and its children.
-    renderer.render(stage);
+  // this is the main render call that makes pixi draw your container and its children.
+  renderer.render(stage);
 }
 
 })
