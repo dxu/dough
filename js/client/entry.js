c7c686e594fc62cafebedf12aa606dc6708a16fd
David <xudavidb@gmail.com>
2016-08-31 15:05:06 -0700

    basic key examples

--- js/client/entry.js
+++ js/client/entry.js
@@ -1,5 +1,6 @@
 import PIXI from 'pixi.js'
 import * as util from './utilities'
+import SpatialHash from './spatialhash'
 
 util.ready(function(){
 
@@ -44,6 +45,7 @@ canvas.addEventListener('keydown', function(evt) {
       break
   }
 
+
   console.log(speed)
 
   if (evt.keyCode === util.keys.A) {
@@ -76,30 +78,58 @@ canvas.addEventListener('keyup', function(evt) {
   console.log(zelda.position.y,zelda.position.x)
 })
 
+
 // You need to create a root container that will hold the scene you want to draw.
 var stage = new PIXI.Container();
 
+let comparator = function() {
+
+}
+
+let bucketHasher = function(item) {
+  return
+
+}
+
+let uuidHasher = function() {
+
+}
+
+let hash = new SpatialHash()
+
+
 
 // load the texture we need
-PIXI.loader.add('zelda', './assets/img/zelda.gif').load(function (loader, resources) {
-  // This creates a texture from a 'zelda.png' image.
-  zelda = new PIXI.Sprite(resources.zelda.texture);
+PIXI.loader.add('zelda', './assets/img/zelda.gif')
+  .add('heart', './assets/img/heart.png')
+  .load(function (loader, resources) {
+    // This creates a texture from a 'zelda.png' image.
+    zelda = new PIXI.Sprite(resources.zelda.texture);
 
-  // Setup the position and scale of the zelda
-  zelda.position.x = 400;
-  zelda.position.y = 300;
+    // Setup the position and scale of the zelda
+    zelda.position.x = 400;
+    zelda.position.y = 300;
 
-  zelda.anchor.x = 0.5;
-  zelda.anchor.y = 0.5;
+    zelda.anchor.x = 0.5;
+    zelda.anchor.y = 0.5;
 
-  zelda.scale.x = 2;
-  zelda.scale.y = 2;
+    zelda.scale.x = 2;
+    zelda.scale.y = 2;
 
-  // Add the zelda to the scene we are building.
-  stage.addChild(zelda);
+    for (var i=0; i<10; i++) {
+      let heart = new PIXI.Sprite(resources.heart.texture);
+      heart.position.x = Math.random() * 800;
+      heart.position.y = Math.random() * 600;
+      heart.anchor.x = 0.5;
+      heart.anchor.y = 0.5;
+      stage.addChild(heart);
+    }
 
-  // kick off the animation loop (defined below)
-  animate();
+    // Add the zelda to the scene we are building.
+    stage.addChild(zelda);
+
+    // kick off the animation loop (defined below)
+    animate();
 });
 
 function animate() {
@@ -107,7 +137,7 @@ function animate() {
   requestAnimationFrame(animate);
 
   // each frame we spin the zelda around a bit
-  zelda.rotation += 0.01;
+  // zelda.rotation += 0.01;
   console.log(speed)
   zelda.position.y += 1 * speed.y;
   zelda.position.x += 1 * speed.x;
