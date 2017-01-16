8c238dde4ba01c42e2b508448d2a06eef960f829
David <xudavidb@gmail.com>
2017-01-15 16:45:32 -0800

    flow-typed add Matter.Composite

--- flow-typed/matter.js
+++ flow-typed/matter.js
@@ -2,11 +2,48 @@ declare module "matter-js" {
 
   declare type BodyOptionsType = Object;
   declare type EngineOptionsType = Object;
+  declare type CompositeOptionsType = {
+    bodies: Array<Body>,
+    composites: Array<Composite>,
+    constraints: Array<Constraint>,
+    id: number,
+    isModified: bool,
+    label: String,
+    parent: Composite,
+    type: String,
+  };
 
-
+  declare type Body = {};
   declare type World = {};
   declare type Grid = {};
+  declare type Constraint = {};
+  declare type Vector = {};
+  declare type Render = {};
 
+  declare class Composite {
+    static add(composite: Composite, object: Object): Composite;
+    static allBodies(composite: Composite): Array<Body>;
+    static allConstraints(composite: Composite): Array<Constraint>;
+    static clear(composite: Composite, keepStatic: bool, deep?: bool): Composite;
+    static create(options?: CompositeOptionsType): Composite;
+    static get(composite: Composite, id: number, type: number): Object;
+    static move(compositeA: Composite, objects: Array<Object>, compositeB: Composite): Composite;
+    static rebase(composite: Composite): Composite;
+    static remove(composite: Composite, object: Object, deep?: bool): Composite;
+    static rotate(composite: Composite, rotation: number, point: Vector, recursive?: bool): void;
+    static scale(composite: Composite, scaleX: number, scaleY: number, point: Vector, recursive?: bool): void;
+    static setModified(composite: Composite, isModified: bool, updateParents?: bool, updateChildren?: bool): void;
+    static translate(composite: Composite, translation: Vector, recursive: bool): void;
+    // CompositeOptionsType
+    bodies: Array<Body>,
+    composites: Array<Composite>,
+    constraints: Array<Constraint>,
+    id: number,
+    isModified: bool,
+    label: String,
+    parent: Composite,
+    type: String,
+  }
 
   declare class Engine {
     static clear(engine: Engine): void;
@@ -21,7 +58,7 @@ declare module "matter-js" {
     enableSleeping: bool,
     positionIterations: number,
     // DEPRECATED!
-    render(): void,
+    render: Render,
     timing: {
       timeScale: number,
       timestamp: number,
